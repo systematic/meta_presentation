d0c1f402918b526abb2f43abb25194244fa57750
Matthew Gordon <gordon@jeremiad.telaranrhiod.com>
2009-05-11 15:48:34 -0400

    this seems like a good starting point

--- start_app/app/models/project.rb
+++ start_app/app/models/project.rb
@@ -1,2 +1,19 @@
 class Project < ActiveRecord::Base
+  TASKS = ["steal_underpants", "dotdotdot", "profit"]
+  
+  def self.tasks
+    Project::TASKS
+  end
+  
+  def steal_underpants
+    "theft!"
+  end
+  
+  def dotdotdot
+    "..."
+  end
+  
+  def profit
+    "$$$$$$$"
+  end
 end
